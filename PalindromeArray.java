421bab0a448e86361ea2b3cb07d7f1f6c3092254
Ankit Upadhyay <ankitjust4u121@gmail.com>
2024-03-13 01:17:21 +0530

    Great Example for Palindrome Array.

---
+++ PalindromeArray.java
@@ -0,0 +1,25 @@
+public class PalindromeArray {
+    public static int palinArray(int [] arr, int n) {
+        for (int i = 0 ; i < n ; i++) {
+            if (!isPalindrome(arr[i])) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    public static boolean isPalindrome(int n) {
+        int original = n;
+        int reversed = 0;
+        while (n > 0) {
+            int reminder = n % 10;
+            reversed = reversed * 10 + reminder;
+            n /= 10;
+        }
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        int result = palinArray(new int[] {111, 222,  333,  444, 555}, 5);
+        System.out.print(result);
+    }
+}
