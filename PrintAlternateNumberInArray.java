7bcf9d516972d2b9fd32efebf3b0461a8f22702a
Ankit Upadhyay <ankitjust4u121@gmail.com>
2024-03-12 19:21:12 +0530

    It is just for test.

--- PrintAlternateNumberInArray.java
+++ PrintAlternateNumberInArray.java
@@ -1,5 +1,5 @@
 public class PrintAlternateNumberInArray {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
