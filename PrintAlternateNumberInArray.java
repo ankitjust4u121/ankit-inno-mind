6bf2e3eecbe0753f2a69ae77c37c362345df1025
Ankit Upadhyay <ankitjust4u121@gmail.com>
2024-03-12 22:38:10 +0530

    Adding alternate number in Array.

--- PrintAlternateNumberInArray.java
+++ PrintAlternateNumberInArray.java
@@ -1,5 +1,10 @@
 public class PrintAlternateNumberInArray {
+    public static void printAlternateNumberInArray(int[] arr, int n) {
+        for (int i = 0; i < n ; i += 2) {
+             System.out.print(arr[i]+" ");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        printAlternateNumberInArray(new int[] {1,2,3,4}, 4);
     }
 }
